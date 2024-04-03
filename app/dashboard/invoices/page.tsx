8485382b3cd8c43407001f415d0aa86e4cb573bd
fungai <fungai.nyatanga16@gmail.com>
2024-04-03 18:20:59 -0400

    added search bar and create button

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,19 @@
-export default function Page(){
-    return <p>Invoices</p>
+import Search from '@/app/ui/search';
+import {CreateInvoice } from '@/app/ui/invoices/buttons';
+
+
+import { lusitana } from '@/app/ui/fonts/fonts';
+
+export default async function Page() {
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice  />
+      </div>
+    </div>
+  );
 }
